12106b7fc40f1ac39fbdb044cf53884c4780d8eb
Erlend Hamnaberg <erlend@hamnaberg.net>
2020-05-13 18:15:53 +0200

    Channel Options

--- src/main/scala/net/hamnaberg/http4s/netty/NettyServerBuilder.scala
+++ src/main/scala/net/hamnaberg/http4s/netty/NettyServerBuilder.scala
@@ -32,11 +32,8 @@ final class NettyServerBuilder[F[_]](
     maxChunkSize: Int,
     transport: NettyTransport,
     banner: immutable.Seq[String],
-    executionContext: ExecutionContext
-    //sslBits: Option[NettySSLConfig],
-    //enableWebsockets: Boolean = false,
-    //maxWSPayloadLength: Option[Int],
-    //nettyChannelOptions: NettyBuilder.NettyChannelOptions
+    executionContext: ExecutionContext,
+    nettyChannelOptions: NettyServerBuilder.NettyChannelOptions
 )(implicit F: ConcurrentEffect[F]) {
   private val logger = org.log4s.getLogger
 
@@ -51,7 +48,8 @@ final class NettyServerBuilder[F[_]](
       maxChunkSize: Int = maxChunkSize,
       transport: NettyTransport = transport,
       banner: immutable.Seq[String] = banner,
-      executionContext: ExecutionContext = executionContext
+      executionContext: ExecutionContext = executionContext,
+      nettyChannelOptions: NettyServerBuilder.NettyChannelOptions = nettyChannelOptions
   ): NettyServerBuilder[F] =
     new NettyServerBuilder[F](
       httpApp,
@@ -64,7 +62,8 @@ final class NettyServerBuilder[F[_]](
       maxChunkSize,
       transport,
       banner,
-      executionContext
+      executionContext,
+      nettyChannelOptions
     )
 
   private def getEventLoop: EventLoopHolder[_ <: ServerChannel] =
@@ -91,13 +90,14 @@ final class NettyServerBuilder[F[_]](
   final def bindLocal(port: Int)                                                  = bindHttp(port, defaults.Host)
   final def bindAny(host: String = defaults.Host)                                 = bindHttp(0, host)
 
-  def withNativeTransport                                      = copy(transport = NettyTransport.Native)
-  def withNioTransport                                         = copy(transport = NettyTransport.Nio)
-  def withoutBanner                                            = copy(banner = Nil)
-  def withMaxHeaderSize(size: Int)                             = copy(maxHeaderSize = size)
-  def withMaxChunkSize(size: Int)                              = copy(maxChunkSize = size)
-  def withMaxInitialLineLength(size: Int)                      = copy(maxInitialLineLength = size)
-  def withServiceErrorHandler(handler: ServiceErrorHandler[F]) = copy(serviceErrorHandler = handler)
+  def withNativeTransport                                                   = copy(transport = NettyTransport.Native)
+  def withNioTransport                                                      = copy(transport = NettyTransport.Nio)
+  def withoutBanner                                                         = copy(banner = Nil)
+  def withMaxHeaderSize(size: Int)                                          = copy(maxHeaderSize = size)
+  def withMaxChunkSize(size: Int)                                           = copy(maxChunkSize = size)
+  def withMaxInitialLineLength(size: Int)                                   = copy(maxInitialLineLength = size)
+  def withServiceErrorHandler(handler: ServiceErrorHandler[F])              = copy(serviceErrorHandler = handler)
+  def withNettyChannelOptions(opts: NettyServerBuilder.NettyChannelOptions) = copy(nettyChannelOptions = opts)
 
   /**
     * Socket selector threads.
@@ -163,12 +163,14 @@ final class NettyServerBuilder[F[_]](
       eventLoop.shutdownGracefully()
       ()
     }
-    def runtimeClass: Class[A]                = classTag.runtimeClass.asInstanceOf[Class[A]]
-    def configure(bootstrap: ServerBootstrap) =
-      bootstrap
+    def runtimeClass: Class[A] = classTag.runtimeClass.asInstanceOf[Class[A]]
+    def configure(bootstrap: ServerBootstrap) = {
+      val configured = bootstrap
         .group(eventLoop)
         .channel(runtimeClass)
         .childOption(ChannelOption.AUTO_READ, java.lang.Boolean.FALSE)
+      nettyChannelOptions.foldLeft(configured) { case (c, (opt, optV)) => c.childOption(opt, optV) }
+    }
 
   }
   case class Bound(address: InetSocketAddress, holder: EventLoopHolder[_ <: ServerChannel], channel: Channel)
@@ -187,8 +189,46 @@ object NettyServerBuilder {
       maxChunkSize = 8192,
       transport = NettyTransport.Native,
       banner = org.http4s.server.defaults.Banner,
-      executionContext = ExecutionContext.global
+      executionContext = ExecutionContext.global,
+      nettyChannelOptions = NettyChannelOptions.empty
     )
+  }
+
+  /** Ensure we construct our netty channel options in a typeful, immutable way, despite
+    * the underlying being disgusting
+    */
+  sealed abstract class NettyChannelOptions {
+
+    /** Prepend to the channel options **/
+    def prepend[O](channelOption: ChannelOption[O], value: O): NettyChannelOptions
+
+    /** Append to the channel options **/
+    def append[O](channelOption: ChannelOption[O], value: O): NettyChannelOptions
+
+    /** Remove a channel option, if present **/
+    def remove[O](channelOption: ChannelOption[O]): NettyChannelOptions
+
+    private[http4s] def foldLeft[O](initial: O)(f: (O, (ChannelOption[Any], Any)) => O): O
+  }
+
+  object NettyChannelOptions {
+    val empty = new NettyCOptions(Vector.empty)
+  }
+
+  private[http4s] final class NettyCOptions(private[http4s] val underlying: Vector[(ChannelOption[Any], Any)])
+      extends NettyChannelOptions {
+
+    def prepend[O](channelOption: ChannelOption[O], value: O): NettyChannelOptions =
+      new NettyCOptions((channelOption.asInstanceOf[ChannelOption[Any]], value: Any) +: underlying)
+
+    def append[O](channelOption: ChannelOption[O], value: O): NettyChannelOptions =
+      new NettyCOptions(underlying :+ ((channelOption.asInstanceOf[ChannelOption[Any]], value: Any)))
+
+    def remove[O](channelOption: ChannelOption[O]): NettyChannelOptions =
+      new NettyCOptions(underlying.filterNot(_._1 == channelOption))
+
+    private[http4s] def foldLeft[O](initial: O)(f: (O, (ChannelOption[Any], Any)) => O) =
+      underlying.foldLeft[O](initial)(f)
   }
 }
 
